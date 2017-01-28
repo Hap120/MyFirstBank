513f1cc8c25b260a6517416ae05eb4c94d6149fb
himali <himalipethe@gmail.com>
2017-01-28 11:02:34 -0800

    My First Code

---
+++ MyFirstBankApp/MyFirstBankApp/Account.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstBankApp
+{
+    /// <summary>
+    /// This class defines the account of a bank
+    
+    
+    class Account
+    {
+        #region Statics
+
+        private static int lastAccountNumber = 0;
+
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// this is a comment for the account number property 
+        /// </summary>
+        public int AccountNumber { get; private set; }
+
+        public string EmailAddress { get; set; }
+
+        public decimal Balance { get; private set; }
+
+        #endregion
+
+
+        #region Methods
+
+        public Account()   // this is the constructor that was hidden , "new" 
+                           //in the program.cs file
+
+        {
+
+            this.AccountNumber = ++lastAccountNumber;
+            //or you can do this ..
+            //lastAccountNumber +=1;
+            //this,AccountNumber = lastAccountNumber += 1;
+        }
+
+        // overloading.. method name is the same, but parameters are different  
+        public Account(string emailAddress)
+           this.AccountNumber = ++lastAccountNumber;
+        this.EmailAddress = ++EmailAddress
+
+
+        public decimal Deposit(decimal amount)
+
+        {
+        //Balance = Balance + amount;
+        Balance += amount;
+        return Balance;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+        Balance -= amount;
+        }
+         #endregion
+    }
+}
---
+++ MyFirstBankApp/MyFirstBankApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstBankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new Account();
+
+            var myAccount = new Account();
+            myAccount.EmailAddress = "test@test.com";   
+            var newBalance = myAccount.Deposit(200.50M);
+            Console.WriteLine($"The balance in my account - {myAccount.AccountNumber} is {myAccount.Balance:C}");
+          
+            // $ is to tell C there is a placeholder... :C is to tell C to put the value in another format
+            //eg money is diplayed with a dollar sign
+            //This is just to show how a new account can be created
+
+            var myAccount2 = new Account();
+            myAccount2.EmailAddress = "test@test.com";
+            newBalance = myAccount2.Deposit(200.50M);
+            Console.WriteLine($"The balance in my account - {myAccount2.AccountNumber} is {myAccount2.Balance:C}");
+
+
+        }
+    }
+}
